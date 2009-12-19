150b20a4b3bca0f435852dd5e0d58d188885f46e
Jade Rubick <jade@rubick.com>
2009-12-19 07:30:37 -0800

    Order forums by thread count

--- app/controllers/forums_forums_controller.rb
+++ app/controllers/forums_forums_controller.rb
@@ -4,6 +4,7 @@ class ForumsForumsController < ApplicationController
 
   def index
     @forums = ForumsForum.find(:all, \
+                               :order => 'thread_count desc', \
                                :conditions => \
                                { :enabled_p => "t", \
                                  :package_id => MAIN_FORUMS } )
