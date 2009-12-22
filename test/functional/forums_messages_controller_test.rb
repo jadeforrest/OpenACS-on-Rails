758e020eb55305151a1b4b83bc8acb10642d103c
Jade Rubick <jade@rubick.com>
2009-12-21 16:24:54 -0800

    Update routing test

--- test/functional/forums_messages_controller_test.rb
+++ test/functional/forums_messages_controller_test.rb
@@ -3,17 +3,16 @@ require 'test_helper'
 class ForumsMessagesControllerTest < ActionController::TestCase
 
   def test_should_route_to_one_message
-    assert_routing '/forums/1/messages/2', { \
+    assert_routing '/messages', { \
       :controller => "forums_messages", \
       :action => "index"}
   end
 
   def test_should_route_to_one_message
-    assert_routing '/forums/1/messages/2', { \
+    assert_routing '/messages/2', { \
       :controller => "forums_messages", \
       :action => "show", \
-      :id => "2", \
-      :forums_forum_id => "1" }
+      :id => "2" }
   end
 
 end
